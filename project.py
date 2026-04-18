a13a0d7c9e4b5f7e9da9b60739def9607dee19e9
xe4ot321 <xeot820@gmail.com>
2026-04-18 16:47:41 +0300

    Implement site and job classes in project.py
    Added classes for site information and job retrieval.

--- project.py
+++ project.py
@@ -1,1 +1,13 @@
+import requests
 
+class saits:
+    def __init__(self,nimi,link,info): #здесь я сохраню всю информацию из разных сайтов
+        self.nimi=nimi
+        self.link=link
+        self.info=info
+
+class töökasa(saits): #здесь инфа töötakassa
+    def getjob():
+        print("")
+
+        
